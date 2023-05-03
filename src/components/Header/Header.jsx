51fb796d66fb0de73228ca5e38896b17c52f27ce
Alexandre Halas <alexandrehalas@gmail.com>
2023-05-03 19:04:59 -0300

    Implementando minha paleta de cores personalizadas no Tailwind

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import ToggleTheme from '../ToggleTheme/ToggleTheme';
 
 export const Header = () => {
   return (
-    <div className="flex h-20 bg-gray-500 justify-between items-center px-5 sm:rounded-xl sm:m-5">
+    <div className="flex h-20 bg-blueSystem-200 dark:bg-graySystem-200 justify-between items-center px-5 sm:rounded-xl sm:m-5">
       <span className="text-gray-100">Olá, usuário</span>
       <h1 className="text-gray-100 text-xl">Halas Newsletter</h1>
       <ToggleTheme />
