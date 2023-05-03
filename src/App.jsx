522353aa653f913deace9145f1c168eb9acfbdc8
Alexandre Halas <alexandrehalas@gmail.com>
2023-05-03 18:06:00 -0300

    Componente Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+import Header from "./components/Header/Header";
+
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">Hello Tailwind CSS</h1>
+    <Header />
   )
 }
 
