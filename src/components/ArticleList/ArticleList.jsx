c45b56f142c089f0306fdbeaeb9bca33bf690dd8
Alexandre Halas <alexandrehalas@gmail.com>
2023-05-04 21:21:44 -0300

    Componente ARticle e aplicado estilos nos componentes Article e ArticleList

--- src/components/ArticleList/ArticleList.jsx
+++ src/components/ArticleList/ArticleList.jsx
@@ -1,9 +1,10 @@
 import articles from '../../../articles.json'
+import Article from '../Article/Article'
 
 const ArticleList = () => {
     return (
-        <div className='mt-5 sm:mt-0 grid gap-5 m-auto max-w-2xl sm:grid-cols-2 lg:grid-cols-3'>
-            {articles.map(article => <p key={article.id}>{article.title}</p>)}
+        <div className='mt-5 sm:mt-0 grid gap-5 m-auto max-w-2xl sm:grid-cols-2 lg:grid-cols-3 lg:max-w-[1000px]'>
+            {articles.map((article) => <Article key={article.id} {...article} />)}
         </div>
     )
 }
